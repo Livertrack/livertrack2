d1403fce32cab16151e30b73b67f7be1fa118ef9
Livertrack <livertrack@proton.me>
2026-03-10 09:53:20 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-}
-module.exports = nextConfig
